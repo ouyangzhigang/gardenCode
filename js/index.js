ea4ef0717c53e1ee63020c1a5ff0a19d65aa4819
liuweichao <270253535@qq.com>
2015-09-02 17:21:19 +0800

    修改了一下午，把基本demo写好

--- js/index.js
+++ js/index.js
@@ -9,9 +9,16 @@ $(document).ready(function () {
     });
 
 
+
+
+
+
     //swiper
     var mySwiper = new Swiper ('.swiper-container', {
         direction : 'vertical',
+        onlyExternal : true,
+        effect:'fade',
+        speed:1000,
         onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
             swiperAnimateCache(swiper); //隐藏动画元素
             swiperAnimate(swiper); //初始化完成开始动画
@@ -26,10 +33,37 @@ $(document).ready(function () {
         }
     });
 
-
+    //page1
     $(".page1_step4").on('click', function () {
        mySwiper.slideNext();
     });
+
+    //page2
+    $(".page2_submit").on('click', function () {
+        mySwiper.slideNext();
+    });
+
+
+    //page3
+    $('.gotoIndex').on('click', function () {
+        mySwiper.slideTo( 0,1000 );
+    });
+    $('.page3_next').on('click', function () {
+        mySwiper.slideNext();
+    });
+
+
+    //page4
+    $('.page4_step3').on('click', function () {
+        mySwiper.slideTo( 1,1000,false );
+    });
+    $('.page4_step4').on('click', function () {
+        alert('分享');
+    });
+    $('.page4_step5').on('click', function () {
+        mySwiper.slideNext();
+    });
+
 
 
 
